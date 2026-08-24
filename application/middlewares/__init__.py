8d2d5b14eadf0851b4591b2be30260b13983baa4
Amirhossein Abbasi <71593058+ahaf138396@users.noreply.github.com>
2026-08-24 10:04:03 +0330

    feat(middlewares): Add CORS middleware module

--- application/middlewares/__init__.py
+++ application/middlewares/__init__.py
@@ -1,6 +1,7 @@
+from cors import add_cors_middleware
 from typing import Optional
 
 async def all_middlewares(app, handler:Optional):
-    pass
+    add_cors_middleware(app)
 
 __all__ = ["all_middlewares"]
